cfbac716c233edbeae8edef95eb2c0fe6c9d448e
Natan Tesfahun <75587079+natantes@users.noreply.github.com>
2023-04-14 06:43:52 -0500

    final commit for pset1

--- PSET1/PSET1.R
+++ PSET1/PSET1.R
@@ -5,41 +5,40 @@ set.seed(32923)
 y <- rnorm(5, mean=0, sd=1)
 y_mat <- matrix(rnorm(5*100000, mean=0, sd=1), 100000, 5)
 y_b <- apply(y_mat, 1, sd)
-y_b
+
 s <- mean(y_b)
-s_0 <- s * sqrt( (length(y_b) - 1) / (length(y_b)) )
-s_1 <- s * sqrt( (length(y_b) - 1) / (length(y_b) - 1.5) )
+s_0 <- s * sqrt( (5 - 1) / (5) )
+s_1 <- s * sqrt( (5 - 1) / (5 - 1.5) )
+cat("estimates: ", s, s_0, s_1)
 
 bias_s <- s - 1
 bias_s_0 <- s_0 - 1
 bias_s_1 <- s_1 - 1
+cat("bias: ", bias_s, bias_s_0, bias_s_1)
 
-bias_s
-bias_s_0
-bias_s_1
+
 
 ### PROBLEM 4
 
-software_data <- read.csv("./stat_352-0/software.csv")
+software_data <- read.csv("./Dev/non_parametric_methods/PSET1/software.csv")
 software_data <- software_data[,1]
 lambda <- 1 / mean(software_data)
 
-y<-seq(from=0,to=10,by= 0.001)
-plot(ecdf(software_data), verticals=T, cex=0.5)
-# plot(pexp(y, rate = lambda), type="l", xlab="y", ylab="F(y)")
+y<-seq(from=0,to=120,by= 0.01)
 
 log_software_data <- log(software_data)
-log_software_data
 
 log_mean <- mean(log_software_data)
 log_sd <- sd(log_software_data)
-plot(plnorm(y, meanlog = log_mean, sdlog = log_sd))
-plot(ecdf(software_data), verticals=T, add=T, cex=0.5)
+plot(y, pexp(y, rate = lambda), type="l", xlab="y", ylab="F(y)")
+plot(ecdf(software_data), verticals=T, cex=0.5, xlab="y", ylab="F(y)", add=T)
+lines(y, plnorm(y, meanlog = log_mean, sdlog = log_sd), type="l", lty=3)
+
 
 
 ### PROBLEM 5
 
-peabody_data <- read.csv("./stat_352-0/Peabody.csv")
+peabody_data <- read.csv("./Dev/non_parametric_methods/PSET1/Peabody.csv")
 peabody_data <- peabody_data[,1]
 
 peabody_mean <- mean(peabody_data)
@@ -60,9 +59,10 @@ boot(peabody_data, Median, 100000)
 boot(peabody_data, Mean_Median_Diff, 100000)
 
 
+
 ### PROBLEM 6
 
-software_data <- read.csv("./stat_352-0/software.csv")
+software_data <- read.csv("./Dev/non_parametric_methods/PSET1/software.csv")
 software_data <- software_data[,1]
 
 software_mean <- mean(software_data)
