f53939321ea43cc11772c61d72fd6000b97162df
natanmoon <75587079+natanmoon@users.noreply.github.com>
2023-04-17 09:05:21 -0500

    updates

--- PSET2/PSET2.R
+++ PSET2/PSET2.R
@@ -1,22 +1,22 @@
 
 
+setwd("/")
+
 ### PROBLEM 1
 
 crustacean_data <- read.csv("./Dev/non_parametric_methods/PSET2/crustaceans.csv")
 crustacean_data <- as.numeric(crustacean_data[,1])
-hist(crustacean_data, breaks=50, freq=F)
-hist(crustacean_data, breaks=30, freq=F)
-hist(crustacean_data, breaks=25, freq=F)
-hist(crustacean_data, breaks=10, freq=F)
+hist(crustacean_data, breaks=20, freq=F)
 
 
 ### PROBLEM 2
 
 software_data <- read.csv("./Dev/non_parametric_methods/PSET2/software.csv")
 software_data <- as.numeric(software_data[,1])
-# hist(software_data, breaks=20, freq=F)
-# hist(software_data,  breaks=10, freq=F)
-hist(software_data,  breaks=5, freq=F)
+hist(software_data, freq=F)
+hist(software_data, breaks=5, freq=F)
+hist(software_data,  breaks=15, freq=F)
+hist(software_data,  breaks=20, freq=F)
 
 ### PROBLEM 4
 
